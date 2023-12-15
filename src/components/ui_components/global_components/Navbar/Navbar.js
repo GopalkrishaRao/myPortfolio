92ba8db204f64f66638d473505e10b968915aeac
GK Rao <hrgkrao@gmail.com>
2023-12-15 21:18:40 +0530

    Nav bar show/hide on scroll

--- src/components/ui_components/global_components/Navbar/Navbar.js
+++ src/components/ui_components/global_components/Navbar/Navbar.js
@@ -1,14 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './navbar.css'; 
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+  
+// Navbar scroll hide/show scolle
+const [prevScrollpos, setPrevScrollpos]=useState(window.pageYOffset);
+useEffect(()=>{
+const handleScroll = ()=>{
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('navbar').style.top = '0';
+    
+  } else {
+    document.getElementById('navbar').style.top = '-250px';
+    // document.getElementById('navbar').style.transition="top 0.4s ease"
+  }
+  setPrevScrollpos(currentScrollPos);
+};
+
+
+window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+},[prevScrollpos])
+
+
+
     function openNav() {
         document.getElementById("mySidenav").style.width = "250px";
         // document.getElementById("main").style.marginRight = "250px";
         document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
       }
+    
       
       function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
@@ -16,9 +43,10 @@ export default function Navbar() {
         document.body.style.backgroundColor = "white";
       }
 
+
   return (
     <>
-    <nav className="navbar">
+    <nav className="navbar" id='navbar'>
             <div className="navbar-left">
                 <div className="navbar-title">Rao</div>
             </div>
